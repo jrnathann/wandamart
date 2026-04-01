2d91c98c89295118f22a899ebe080ff646b58ef4
jordanjunior22 <jordan.kheambo@final.edu.tr>
2026-04-02 00:19:08 +0100

    update

---
+++ app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+import { storeConfig } from "@/data/configData";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: `${storeConfig.name}`,
+    short_name: "Admin",
+    description: "Administration Dashboard",
+    start_url: "/admin",
+    scope: "/admin",
+    display: "standalone",
+    background_color: storeConfig.theme.colors.black,
+    theme_color: storeConfig.theme.colors.black,
+    icons: [
+      {
+        src: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  };
+}
